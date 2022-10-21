74c72f192afbcc3080a5931816f2a1956d271f05
Paulo Marques <pauloedums@gmail.com>
2022-10-20 23:07:38 -0300

    First commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const UsersRouter = require('./routes/users');
+module.exports = () => {
+    const app = express();
+
+    app.use(express.json());
+    app.use(UsersRouter);
+
+    return app;
+}
